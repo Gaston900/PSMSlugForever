66e5f782256e0dc54451f1373081b91d6cad2b09
unknownplayers940 <unknownplayers940@gmail.com>
2024-02-25 12:04:04 -0300

    PSMSlugForever (Metal Slug MVS Gold Collection Edition 4.9)

--- src/osd/winui/winui.h
+++ src/osd/winui/winui.h
@@ -84,7 +84,7 @@
 #endif
 #define MAMENAME			"PSMSlugForever"
 
-#define MAME_VERSION		"4.8"
+#define MAME_VERSION		"4.9"
 #define MAMEUIFX_VERSION	MAME_VERSION " (" __DATE__")"
 
 #define SEARCH_PROMPT		"<search here>"
