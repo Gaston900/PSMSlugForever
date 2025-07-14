5905ffad40c691bf0c60217e9cc13e3ac4cdcec9
unknownplayers940 <unknownplayers940@gmail.com>
2025-07-13 21:18:38 -0300

    Fix

--- src/osd/winui/winui.h
+++ src/osd/winui/winui.h
@@ -84,7 +84,7 @@
 #endif
 #define MAMENAME			"PSMSlugForever"
 
-#define MAME_VERSION		"5.5"
+#define MAME_VERSION		"5.6"
 #define MAMEUIFX_VERSION	MAME_VERSION " (" __DATE__")"
 
 #define SEARCH_PROMPT		"<search here>"
