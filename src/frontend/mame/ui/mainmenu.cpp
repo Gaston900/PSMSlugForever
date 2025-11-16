f325ea23c945114de67c9213dcfe1e2e58326066
unknownplayers940 <unknownplayers940@gmail.com>
2025-11-16 17:06:24 -0300

    .

--- src/frontend/mame/ui/mainmenu.cpp
+++ src/frontend/mame/ui/mainmenu.cpp
@@ -149,22 +149,24 @@ void menu_main::populate(float &customtop, float &custombottom)
 //	item_append(menu_item_type::SEPARATOR);
 
 	/* add favorite menu */
-//	if (!mame_machine_manager::instance()->favorite().isgame_favorite())
+//	if (!mame_machine_manager::instance()->favorite().is_favorite(machine()))
 //		item_append(_("Add To Favorites"), "", 0, (void *)ADD_FAVORITE);
 //	else
 //		item_append(_("Remove From Favorites"), "", 0, (void *)REMOVE_FAVORITE);
 
-	item_append(menu_item_type::SEPARATOR);
-
-	item_append(_("Quit from Machine"), "", 0, (void *)QUIT_GAME);
-//	item_append(string_format(_("About %s"), emulator_info::get_appname()), "", 0, (void *)ABOUT);
-
 //	item_append(menu_item_type::SEPARATOR);
 
-//  item_append(_("Quit from Machine"), nullptr, 0, (void *)QUIT_GAME);
+//	item_append(_("Quit from Machine"), "", 0, (void *)QUIT_GAME);
+//	item_append(string_format(_("About %s"), emulator_info::get_appname()), "", 0, (void *)ABOUT);
+
+	item_append(menu_item_type::SEPARATOR);
 
 	/* add reset and exit menus */
 	item_append(_("Select New Machine"), "", 0, (void *)SELECT_GAME);
+
+    item_append(_("Quit from Machine"), "", 0, (void *)QUIT_GAME);
+
+	item_append(menu_item_type::SEPARATOR);
 }
 
 menu_main::~menu_main()
